a43dc1fc30eab2fedd7240f37d75730d7f0af817
VictorRodriguez <vm.rod25@gmail.com>
2017-09-19 07:43:03 -0500

    Add functionality for rules
    Use simple rules functionality with regex
    
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- combination_map/combination.py
+++ combination_map/combination.py
@@ -1,6 +1,8 @@
+import re
 dict_cpu = {}
 dict_ram = {}
 dict_os = {}
+list_comb = []
 
 def read_file(filename):
     d = {}
@@ -16,14 +18,29 @@ def create_comb():
             for key_os, value_os in dict_os.items():
                 comb = key_cpu + key_ram + key_os
                 print(comb)
+                list_comb.append(str(comb))
     print("\n")
+
+def apply_rules():
+    rules = read_file("rules.txt")
+    if rules:
+        for key_rule, value_rule in rules.items():
+            rule = str(value_rule)
+            print("\nRule: "+ rule + "\n")
+            p = re.compile(rule)
+            for element in list_comb:
+                if p.match(element):
+                    print(element)
 
 if __name__ == "__main__":
     dict_cpu = read_file("cpu.txt")
     dict_os = read_file("os.txt")
     dict_ram = read_file("ram.txt")
-    print("Combinations:\n")
+    print("Total Combinations:\n")
     create_comb()
     total_comb = len(dict_cpu)*len(dict_os)*len(dict_ram)
     print("Total number of comb: " + str(total_comb))
+
+    print("\nCombinations with rules")
+    apply_rules()
 
