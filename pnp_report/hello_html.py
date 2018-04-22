78d9e5e70bdeb90af0078a6395d3b121852ed23b
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-04-22 18:18:56 +0000

    get latest clr release
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- pnp_report/hello_html.py
+++ pnp_report/hello_html.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
- 
+
+import requests
+from bs4 import BeautifulSoup
+
+
 @app.route("/")
 def hello():
-    animal = "dog"
-    return render_template('index.html', value=animal)
+    build_url="https://download.clearlinux.org/current/latest"
+    current_build = requests.get(build_url).text
+    return render_template('index.html', current_build=current_build)
 
 if __name__ == "__main__":
     #app.run()
