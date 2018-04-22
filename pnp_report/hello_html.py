ccfcf56eec1792d0a269d23bed22c119d1ba8678
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-04-22 18:36:05 +0000

    get date and status
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- pnp_report/hello_html.py
+++ pnp_report/hello_html.py
@@ -3,15 +3,33 @@ app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
 
 @app.route("/")
 def hello():
+
     build_url="https://download.clearlinux.org/current/latest"
     current_build = requests.get(build_url).text
-    return render_template('index.html', current_build=current_build)
+
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d")
+   
+    status = get_status()
+
+    render = render_template('index.html', \
+            current_build=current_build, \
+            date=date,\
+            status=status)
+    
+    return render
+
+
+def get_status():
+    # based on the % of perfomrance we decide if the build pass or not form the
+    # PNP perspective
+    return "PASS"
 
 if __name__ == "__main__":
-    #app.run()
     app.run(host='0.0.0.0', port=8080, debug=True)
 
