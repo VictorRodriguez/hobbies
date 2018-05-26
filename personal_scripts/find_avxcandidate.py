4e971e774d8257f8e57d67eac4fd9d5c6fae30f2
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-05-26 23:20:32 +0000

    find if we have an arithmetic sse instruction
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- personal_scripts/find_avxcandidate.py
+++ personal_scripts/find_avxcandidate.py
@@ -1,14 +1,46 @@
 import subprocess
 import argparse
 
+sse_instructions_xmm = []
 
-def analyse_avx(line):
-    if "ymm" in line:
-        print("ymm registers = OK")
-        return True
+sse_instructions_xmm.append("paddb")
+sse_instructions_xmm.append("paddd")
+sse_instructions_xmm.append("paddsb")
+sse_instructions_xmm.append("paddsw")
+sse_instructions_xmm.append("paddusb")
+sse_instructions_xmm.append("paddusw")
+sse_instructions_xmm.append("paddw")
+sse_instructions_xmm.append("pmaddwd")
+sse_instructions_xmm.append("pmulhw")
+sse_instructions_xmm.append("pmullw")
+sse_instructions_xmm.append("psubb")
+sse_instructions_xmm.append("psubsb")
+sse_instructions_xmm.append("psubsw")
+sse_instructions_xmm.append("psubusb")
+sse_instructions_xmm.append("paddusw")
+sse_instructions_xmm.append("paddw")
+sse_instructions_xmm.append("pmaddwd")
+sse_instructions_xmm.append("pmulhw")
+sse_instructions_xmm.append("pmullw")
+sse_instructions_xmm.append("psubb")
+sse_instructions_xmm.append("psubd")
+sse_instructions_xmm.append("psubd")
+sse_instructions_xmm.append("psubsb")
+sse_instructions_xmm.append("psubsw")
+sse_instructions_xmm.append("psubusb")
+sse_instructions_xmm.append("psubusw")
+sse_instructions_xmm.append("psubw")
+
+
+
+
+
+def analyse_sse(line):
     if "xmm" in line:
-        print("xmm registers = OK")
-        return True
+        for sse_inst in sse_instructions_xmm:
+            if sse_inst in line:
+                print(line)
+                return True
 
 def main():
 
@@ -22,13 +54,16 @@ def main():
     alllines = out.decode("latin-1")
     lines =  alllines.split("\n")
     
+
+    sse_cnt = 0
+
     for line in lines:
         if args.verbose:
             print(line)
-        if analyse_avx(line):
-            print("System already build for AVX")
-            break
+        if analyse_sse(line):
+            sse_cnt+=1
 
+    print(sse_cnt)
 if __name__ == '__main__':
     main()
 
