def157723e735a8a4d1845b409cce5da56cc5fb8
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-08-09 19:37:25 +0000

    it works with 256 lng of arrays
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- c_programing_exercises/asm/add_i_avx512.c
+++ c_programing_exercises/asm/add_i_avx512.c
@@ -7,13 +7,13 @@
 
 
 u_int32_t *arr_a, *arr_b, *arr_c;
-int n = 16;
+int N = 256;
 
 void fill_arrays(){
-    arr_a = _mm_malloc(sizeof(u_int32_t) * n, 64);
-    arr_b = _mm_malloc(sizeof(u_int32_t) * n, 64);
-    arr_c = _mm_malloc(sizeof(u_int32_t) * n, 64);
-    for(int i = 0; i < n; i++) {
+    arr_a = _mm_malloc(sizeof(u_int32_t) * N, 64);
+    arr_b = _mm_malloc(sizeof(u_int32_t) * N, 64);
+    arr_c = _mm_malloc(sizeof(u_int32_t) * N, 64);
+    for(int i = 0; i < N; i++) {
         arr_a[i] = 1;
         arr_b[i] = 2;
     }
@@ -23,19 +23,19 @@ void foo(){
 
     __m512i m_a, m_b, m_c;
 
-    m_a = _mm512_load_epi32(arr_a);
-    m_b = _mm512_load_epi32(arr_b);
+    for( int i = 0; i < N; i+=16){
+        m_a = _mm512_load_epi32(&arr_a[i]);
+        m_b = _mm512_load_epi32(&arr_b[i]);
 
-    m_c = _mm512_add_epi32(m_a, m_b);
-
-    _mm512_store_epi32(arr_c, m_c);
-
+        m_c = _mm512_add_epi32(m_a, m_b);
+        _mm512_store_epi32(&arr_c[i], m_c);
+    }
 }
 
 int check_result(){
 
     int ret = 0;
-    for (int i=0; i<n; i++){
+    for (int i=0; i<N; i++){
         if (arr_c[i] == 3)
             continue;
         else
