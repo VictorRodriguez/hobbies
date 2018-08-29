cd8d168735e80ec641e5aa6ffb3b00a7604ddc4b
VictorRodriguez <vm.rod25@gmail.com>
2018-08-29 13:33:19 -0500

    regressions automatic in plot
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- regresion_algorithms/simple_2_points.py
+++ regresion_algorithms/simple_2_points.py
@@ -7,6 +7,7 @@ e = np.array([1.2, 1.1, 0.9, 1.5, 1.2,1.1])
 
 
 # detect regresion 
+regressions = []
 count = 0
 for value in np.nditer(x):
     if (count >= 1):
@@ -14,12 +15,14 @@ for value in np.nditer(x):
         min_limit = y[count] -e[count]
         previous_limit = y[count -1] + e[count -1]
         if (min_limit > previous_limit):
+            regressions.append(count)
             print("LIB: regresion in value %s with regards to previus value %s" % 
                     (str(y[count]),str(y[count-1])))
     count+=1
 
 plt.errorbar(x, y, e, linestyle='-.', marker='*')
-plt.annotate("regresion",(x[5], y[5]))
+for position in regressions:
+    plt.annotate("regresion",(x[position], y[position]))
 
 plt.savefig('image.png')
 plt.show()
