e855f536ed1c36d02b0db5b51a68bac1a514a791
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-10-08 20:01:03 +0000

    fix problem with logc
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- dev_ops/test_master.sh
+++ dev_ops/test_master.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 CGIT_PATH=/home/vrodri3/cgit-repos
-WORK_PATH=$(pwd)
-echo "" > results_fetch.csv
+CGIT_LOG=/tmp/cgit-checker.log
+echo "" > /tmp/results_fetch.csv
+echo `date` > $CGIT_LOG
 for f in $CGIT_PATH/*;do
-    echo "Updating master of $f"
+    echo "Updating master of $f" >> $CGIT_LOG
     cd $f
     PKG=$(pwd | cut -d'/' -f5 | cut -d'.' -f1)
     git config remote.origin.fetch 'refs/heads/*:refs/heads/*' &>/dev/null
     git --bare fetch &>/dev/null
     ret=$?
     if [ $ret -ne 0 ]; then
-        echo "ERROR: master branch does not merge with CLR master"
-        status_flag=1
+        echo "ERROR: master branch does not merge with CLR master" >> $CGIT_LOG
+        status_flag_master=1
     else
-        status_flag=0
+        status_flag_master=0
     fi
     cd -
-    echo "Check merge of devel/master of $PKG"
+    echo "Check merge of devel/master of $PKG" >> CGIT_LOG
     cd /tmp/git-workspace
     rm -rf $PKG
     git clone $f &>/dev/null
@@ -27,16 +28,22 @@ for f in $CGIT_PATH/*;do
         git merge origin/devel &>/dev/null
         ret=$?
         if [ $ret -ne 0 ]; then
-            echo "ERROR: devel branch does not merge with CLR master"
-            status_flag=1
+            echo "ERROR:devel doesn't merge with clr master" >> $CGIT_LOG
+            status_flag_merge=1
         else
-            status_flag=0
+            status_flag_merge=0
         fi
     else
-        echo "WARNING: devel branch does not exist: pass"
-        status_flag=0
+        echo "WARNING: devel branch does not exist: pass" >> $CGIT_LOG
+        status_flag_merge=0
     fi
     git reset --merge
     cd $WORK_PATH
-    echo "$PKG,$status_flag" >> $WORK_PATH/results_fetch.csv
+    echo debug $status_flag_master $status_flag_merge >> $CGIT_LOG
+    if [ $status_flag_master -eq 1 ] || [ $status_flag_merge -eq 1 ]; then
+        echo "ERROR: in $PKG" >> $CGIT_LOG
+        echo "entre $PKG,1" >> /tmp/results_fetch.csv
+    else
+        echo "entre $PKG,0" >> /tmp/results_fetch.csv
+    fi
 done
