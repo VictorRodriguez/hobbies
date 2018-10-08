682a4718240e47a85bc2a4a480997dfb1d6174af
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-10-08 18:56:03 +0000

    check merge of master with devel
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- dev_ops/test_master.sh
+++ dev_ops/test_master.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 CGIT_PATH=/home/vrodri3/cgit-repos
 WORK_PATH=$(pwd)
 echo "" > results_fetch.csv
@@ -8,13 +7,36 @@ for f in $CGIT_PATH/*;do
     echo "Updating master of $f"
     cd $f
     PKG=$(pwd | cut -d'/' -f5 | cut -d'.' -f1)
-    echo $PKG
-    git config remote.origin.fetch 'refs/heads/*:refs/heads/*'
-    git --bare fetch
+    git config remote.origin.fetch 'refs/heads/*:refs/heads/*' &>/dev/null
+    git --bare fetch &>/dev/null
     ret=$?
     if [ $ret -ne 0 ]; then
         echo "ERROR: master branch does not merge with CLR master"
-        echo "$PKG,1" >> $WORK_PATH/results_fetch.csv
+        status_flag=1
+    else
+        status_flag=0
     fi
+    cd -
+    echo "Check merge of devel/master of $PKG"
+    cd /tmp/git-workspace
+    rm -rf $PKG
+    git clone $f &>/dev/null
+    cd $PKG
+    git branch -a | grep 'devel' &> /dev/null
+    if [ $? == 0 ]; then
+        git merge origin/devel &>/dev/null
+        ret=$?
+        if [ $ret -ne 0 ]; then
+            echo "ERROR: devel branch does not merge with CLR master"
+            status_flag=1
+        else
+            status_flag=0
+        fi
+    else
+        echo "WARNING: devel branch does not exist: pass"
+        status_flag=0
+    fi
+    git reset --merge
+    cd $WORK_PATH
+    echo "$PKG,$status_flag" >> $WORK_PATH/results_fetch.csv
 done
-
