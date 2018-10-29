32fbea662b8ac484eb973cb14e3abd50032b293b
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-10-29 07:42:55 -0600

    make stx autospec dashboard
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

---
+++ dev_ops/stx-autospec-dashboard/main.py
@@ -0,0 +1,68 @@
+import wget
+import os
+import git
+import shutil
+
+
+def clone_autospec():
+    gitrepo="https://github.com/clearlinux/autospec.git"
+    local_repo_path='/tmp/autospec'
+    if os.path.isdir(local_repo_path):
+        shutil.rmtree(local_repo_path)
+    git.Repo.clone_from(gitrepo,local_repo_path, depth=1)
+
+def clone_repo(pkg):
+    gitrepo = 'http://starlingx-koji.zpn.intel.com/cgit/packages/' + pkg 
+    local_repo_path='/tmp/' + pkg
+    if os.path.isdir(local_repo_path):
+        shutil.rmtree(local_repo_path)
+    git.Git("/tmp/").clone(gitrepo)
+
+def check_autospec(pkg):
+    local_repo_path='/tmp/' + pkg
+    savedpath = os.getcwd()
+    os.chdir(local_repo_path)
+    cmd = 'python ../autospec/autospec/autospec.py'
+    ret = os.system(cmd)
+    return ret
+
+def main():
+
+    data = {}
+    pkgs = []
+
+    filename='/tmp/stx_pkg.log'
+    url='http://starlingx-koji.zpn.intel.com/misc/projects.list'
+
+
+    if os.path.isfile(filename):
+        pass
+    else:
+        filename = wget.download(url=url,out=filename)
+
+    if os.path.isfile(filename):
+        with open(filename) as f:
+            lines = f.readlines()
+            for line in lines:
+                if 'packages' in line and 'common' not in line \
+                        and 'projects' not in line:
+                    pkg = line.split("/")[1].strip()
+                    pkg = pkg.replace(".git","")
+                    pkgs.append(pkg)
+
+    print("Cloning autospec")
+    clone_autospec()
+
+    for pkg in pkgs:
+        print("Clonning: " + pkg)
+        clone_repo(pkg)
+        if check_autospec(pkg):
+            print(pkg + "Fail")
+            data[pkg] = "Fail"
+        else:
+            print(pkg + "Pass")
+            data[pkg] = "Pass"
+    print(data)
+
+if __name__ == '__main__':
+    main()
