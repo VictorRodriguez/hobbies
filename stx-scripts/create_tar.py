70b77d3f27ddd6cc49e7d246aa930aca085727a7
VictorRodriguez <vm.rod25@gmail.com>
2019-05-23 10:43:05 -0500

    some fixes
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- stx-scripts/create_tar.py
+++ stx-scripts/create_tar.py
@@ -13,18 +13,43 @@ for line in content:
         cmd = "git clone --depth 1 " + line
         os.system(cmd)
 
-dirs = []
-
 proc = subprocess.Popen(['find','.','-d','-name','centos'],stdout=subprocess.PIPE)
+
+src_dirs = []
+pkg_info_files = []
+
 while True:
+
+
     line = proc.stdout.readline()
     if line != '':
         dir_path =  line.rstrip().replace("centos","")
-        dirs.append(dir_path)
         for item in os.listdir(dir_path):
-            if os.path.isdir(os.path.join(dir_path, item)):
+            if os.path.isdir(os.path.join(dir_path,item)):
                 if "centos" not in item:
-                    print(os.path.join(dir_path, item))
+                    src_dir = os.path.join(dir_path,item)
+                    if src_dir in src_dirs:
+                        pass
+                    else:
+                        src_dirs.append(src_dir)
+                    #print(src_dir)
+        for item in os.listdir(dir_path):
+            if os.path.isfile(os.path.join(dir_path, item)):
+                if "PKG-INFO" in item:
+                    pkg_info_file= os.path.join(dir_path,item)
+                    if pkg_info_file in pkg_info_files:
+                        pass
+                    else:
+                        pkg_info_files.append(pkg_info_file)
+                    #print(pkg_info_file)
     else:
         break
 
+for item in pkg_info_files:
+    print(item)
+
+print()
+print()
+
+for item in src_dirs:
+    print(item)
