46fbc2f816e7654ae258e59648d0fa7d4f0c46e8
VictorRodriguez <vm.rod25@gmail.com>
2019-05-25 11:36:01 -0500

    cleaner version
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- stx-scripts/create_tar.py
+++ stx-scripts/create_tar.py
@@ -1,55 +1,56 @@
 import os
 import subprocess
+import re
+
 
 fname = 'repos'
 
-with open(fname) as f:
-    content = f.readlines()
+def grep(file_path,word):
+    FILE = open(file_path, "r")
+    for line in FILE:
+        if re.search(word, line):
+            return line
+    return None
 
-for line in content:
-    line = line.strip()
-    directory = line.split("/")[-1]
-    if (not os.path.isdir(directory)):
-        cmd = "git clone --depth 1 " + line
-        os.system(cmd)
+def clone_repos():
+    with open(fname) as f:
+        content = f.readlines()
+    for line in content:
+        line = line.strip()
+        directory = line.split("/")[-1]
+        if (not os.path.isdir(directory)):
+            cmd = "git clone --depth 1 " + line
+            os.system(cmd)
 
-proc = subprocess.Popen(['find','.','-d','-name','centos'],stdout=subprocess.PIPE)
 
-src_dirs = []
-pkg_info_files = []
+def find_pkgs_to_build(patern):
 
-while True:
+    proc = subprocess.Popen(['find','.','-d','-name',patern]
+        ,stdout=subprocess.PIPE)
 
+    pkg_info_files = []
 
     line = proc.stdout.readline()
-    if line != '':
-        dir_path =  line.rstrip().replace("centos","")
-        for item in os.listdir(dir_path):
-            if os.path.isdir(os.path.join(dir_path,item)):
-                if "centos" not in item:
-                    src_dir = os.path.join(dir_path,item)
-                    if src_dir in src_dirs:
-                        pass
-                    else:
-                        src_dirs.append(src_dir)
-                    #print(src_dir)
-        for item in os.listdir(dir_path):
-            if os.path.isfile(os.path.join(dir_path, item)):
-                if "PKG-INFO" in item:
-                    pkg_info_file= os.path.join(dir_path,item)
-                    if pkg_info_file in pkg_info_files:
-                        pass
-                    else:
-                        pkg_info_files.append(pkg_info_file)
-                    #print(pkg_info_file)
-    else:
-        break
+    while line != '':
+        line = proc.stdout.readline().strip()
+        pkg_info_files.append(line)
 
-for item in pkg_info_files:
-    print(item)
+    return pkg_info_files
 
-print()
-print()
+def main():
+    clone_repos()
+    pkg_info_files = find_pkgs_to_build("PKG-INFO")
+    pkg_info_files = filter(None, pkg_info_files)
+    for item in pkg_info_files:
+        pkg_info = {}
+        pkg_info["pkg_info_path"] = item
+        if (grep(item,"Name")):
+            pkg_info["pkg_name"] = pkg_name = grep(item,"Name").\
+                split(":")[1].strip()
 
-for item in src_dirs:
-    print(item)
+        if (grep(item,"Version")):
+            pkg_info["pkg_version"] = pkg_name = grep(item,"Version").\
+                split(":")[1].strip()
+if __name__== "__main__":
+    main()
+
