e0efdc3c515031bd29e4207384dc14026c5fb555
VictorRodriguez <vm.rod25@gmail.com>
2019-06-07 10:35:16 -0500

    better but not final version , still put cpus to 100%
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- c_programing_exercises/asm/nop_test/main.c
+++ c_programing_exercises/asm/nop_test/main.c
@@ -1,4 +1,36 @@
+#include <pthread.h>
+#include <unistd.h>
+
+
+#define LOOPS 1000000000000
+#define THREADS 48
+
+void *foo(){
+
+    /* If you want to stress the cores
+     *
+        for (int x=0; x<LOOPS; x++){
+        int a = 0;
+        int b = 1;
+        int c = 1;
+            a = b+c;
+    	}
+    */
+    while(1){
+    __asm__("nop");
+    }
+}
+
 int main(){
 
+    pthread_t tid;
+    for (int i = 0; i < THREADS; i++){
+        pthread_create(&tid, NULL,foo,NULL);
+	}
+
+    sleep(5);
     return 0;
+
+    //pthread_exit(NULL);
+	//__asm__("ud2");
 }
