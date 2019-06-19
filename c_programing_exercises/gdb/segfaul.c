a6aa431201e8806180e0f730e6a3206f7f2fc0ba
VictorRodriguez <vm.rod25@gmail.com>
2019-06-19 17:51:50 -0500

    improve
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- c_programing_exercises/gdb/segfaul.c
+++ c_programing_exercises/gdb/segfaul.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main() {
+void foo(){
 	char *str;
 	/* Stored in read only part of data segment */
 	str = "GfG";
 	/* Problem:  trying to modify read only memory */
 	*(str+1) = 'n';
+}
+int main() {
+    foo();
 	return 0;
 }
