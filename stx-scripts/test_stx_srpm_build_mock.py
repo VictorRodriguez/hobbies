c1cc94e24da16d95c93eb5bdff35a29a83d30bc6
VictorRodriguez <vm.rod25@gmail.com>
2019-08-24 20:59:26 -0700

    good version to build and log
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- stx-scripts/test_stx_srpm_build_mock.py
+++ stx-scripts/test_stx_srpm_build_mock.py
@@ -1,18 +1,25 @@
 import sys
 import os
-from os import path
+import glob
+import shutil
 
+TMP="/tmp/rpmbuild"
 INPUT_FILE="stx_srpms.txt"
 CENGN_REPO="http://mirror.starlingx.cengn.ca/mirror/starlingx/master/centos/latest_build/outputs/SRPMS/std/"
-
+DEST_DIR=os.path.join(os.getcwd(),"rpms")
 
 def wget_srpm(srpm):
     url = CENGN_REPO + srpm.strip()
-    cmd = "wget %s" % (url)
+    cmd = "wget -P %s %s" % (TMP,url)
     ret = os.system(cmd)
     if ret:
         print("Error in wget")
         sys.exit(-1)
+
+
+os.system("echo > errors.log")
+if not os.path.exists(DEST_DIR):
+    os.mkdir(DEST_DIR)
 
 F = open(INPUT_FILE,"r")
 lines = F.readlines()
@@ -20,14 +27,17 @@ lines = F.readlines()
 for srpm in lines:
     srpm = srpm.strip()
     print(srpm)
-    if not path.exists(srpm):
+    if not os.path.exists(os.path.join(TMP,srpm)):
         wget_srpm(srpm)
 
-    cmd = "cp %s /tmp/rpmbuild/" % (srpm)
+    cmd = "make srpm SRPM=%s MOCK_CONFIG=local-centos-7-x86_64" % (srpm)
     ret = os.system(cmd)
     if ret:
-        print("Error in cp")
-        sys.exit(-1)
+        print("Error in build w/ mock. RET = %d" % (ret))
+        os.system("echo %s >> errors.log" % (srpm))
 
-
-
+    output = os.path.join(TMP,"output/local-centos-7-x86_64")
+    files = glob.iglob(os.path.join(output, "*.rpm"))
+    for file in files:
+        if os.path.isfile(file):
+            shutil.copy2(file, DEST_DIR)
