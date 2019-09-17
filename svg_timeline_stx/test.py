a7d6453ea1a5f112ce759aa90462e374bfcb76e0
VictorRodriguez <vm.rod25@gmail.com>
2019-09-16 19:46:50 -0500

    final changes for pretty print
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- svg_timeline_stx/test.py
+++ svg_timeline_stx/test.py
@@ -27,13 +27,16 @@ def time_normalize(date):
     seconds = float(date.split(":")[2]) - (ms/1000)
 
     date_ms = (seconds)*1000 + (minutes)*60000 + (hour*3600000) + ms
-    return date_ms / 1000 # return in seconds
+    return int(date_ms / 1000) # return in seconds
 
 def main():
 
     event_id = 0
     start_time = 0.0
     end_time = 0.0
+
+    line = '{:>12}  {:>12}  {:>12} {:>12}'.format("event_id","start_time","end_time","event")
+    print(line)
 
     with open(log, 'r') as fin:
         lines = fin.readlines()
@@ -46,7 +49,7 @@ def main():
 
                 date = (clean_line.split(" ")[1])
                 start_time = (time_normalize(date))
-
+                event = clean_line[30:-1]
                 try:
                     next_clean_line = (lines[event_id + 1]).strip()
                 except IndexError:
@@ -61,21 +64,22 @@ def main():
                     end_time = start_time
                     start_time = cp_end_time
 
-                if start_time == start_time:
-                    start_time = start_time
-                    end_time = end_time + 0.01
+                if start_time == end_time:
+                    continue
+                else:
+                    text = "task_%s" % (event_id)
+                    data['segments'].append({
+                        'group': event_id,
+                        'text': "'%s'" % (text),
+                        'time_start':start_time,
+                        'time_end': end_time
+                    })
 
-                text = "task_%s" % (event_id)
-                data['segments'].append({
-                    'group': event_id,
-                    'text': "'%s'" % (text),
-                    'time_start':start_time,
-                    'time_end': end_time
-                })
+                    line = '{:>12}  {:>12}  {:>12} {:>20}'.format(event_id,date,next_date,event[0:50])
+                    print(line)
 
-                #delta = end_time - start_time
-                #print("%d   %s : %s :  %s" % (event_id,date,next_date,delta))
-    print(json.dumps(data))
+    with open('res.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 if __name__ == "__main__":
     main()
