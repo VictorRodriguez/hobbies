0a11d4536c1779205f099a5dedb7ef6253fe1e6e
VictorRodriguez <vm.rod25@gmail.com>
2019-11-27 10:56:05 -0600

    clean up 2
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- dev_ops/random/cirros_qcow2_creator.sh
+++
@@ -1,4 +0,0 @@
-source openrc
-wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
-qemu-img convert -f qcow2 -O raw cirros-0.3.4-x86_64-disk.img cirros-0.3.4-x86_64-disk.raw
-openstack image create --public --disk-format raw --file ./cirros-0.3.4-x86_64-disk.raw cirros-cloud
