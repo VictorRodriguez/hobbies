60ca955a0179d8f6a328f96c4daa7a4df06575ac
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2019-11-27 19:06:30 +0000

    Draft to detect if cve has lp assigned
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- cves_handling_tool/lp.py
+++ cves_handling_tool/lp.py
@@ -1,11 +1,16 @@
-cachedir = "/home/vrodri3/.launchpadlib/cache/"
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2019 Intel Corporation
+#
 
-import sys
+"""
+Implement system to detect if CVEs has launchpad assigned
+"""
+import json
+from os import path
 from launchpadlib.launchpad import Launchpad
 
-launchpad = Launchpad.login_anonymously\
-    ('lplib.cookbook.json_fetcher', 'production', cachedir, version = 'devel')
-project = launchpad.projects['starlingx']
 
 # These filter the types of bugs open)
 STATUSES = [
@@ -18,33 +23,63 @@ STATUSES = [
     'Fix Released',
 ]
 
-bugs_cves = []
-bug_dic={}
+CVES_FILE = 'cves_open.json'
 
-def get_lps_for_cves():
-    tasks = project.searchTasks(status = STATUSES)
+def get_cves_file(file_name):
+    data = []
+    if path.isfile(file_name):
+        data = json.load(open(file_name, "r"))
+    return data
+
+def search_upstrem_lps():
+    data = []
+    cachedir = "/home/vrodri3/.launchpadlib/cache/"
+    launchpad = Launchpad.login_anonymously\
+        ('lplib.cookbook.json_fetcher', 'production',\
+        cachedir, version='devel')
+    project = launchpad.projects['starlingx']
+    tasks = project.searchTasks(status=STATUSES)
     for task in tasks:
         bug = task.bug
-        if "CVE" in bug.title or "cve" in bug.title:
+        if ("CVE" in bug.title or "cve" in bug.title):
+            bug_dic = {}
             bug_dic['id'] = bug.id
-            bug_dic['title'] =  bug.title
+            bug_dic['title'] = bug.title
             bug_dic['link'] = bug.self_link
-            bugs_cves.append(bug_dic)
+            data.append(bug_dic)
+
+    with open(CVES_FILE, 'w') as outfile:
+        json.dump(data, outfile)
+    return data
+
+def find_cve(cve_id, data):
+    for bug in data:
+        if cve_id in bug["title"]:
+            return bug
+    return None
 
 def cve_assigned(cve_id):
-    if not bugs_cves:
-        sys.exit(-1)
-    for bug in bugs_cves:
-        print (bug)
-    return False
-
+    data = get_cves_file(CVES_FILE)
+    bug = find_cve(cve_id, data)
+    if bug:
+        ret = bug
+    else:
+        data = search_upstrem_lps()
+        bug = find_cve(cve_id, data)
+        if bug:
+            ret = bug
+        else:
+            ret = None
+    return ret
 
 def main():
-    cve_id = "CVE-2018-1002105"
-    get_lps_for_cves()
-    ret = cve_assigned(cve_id)
-    if ret:
-        print("%s,%s"%(cve_id,ret))
 
-if __name__== "__main__":
+    cve_id = "CVE-2018-15686"
+    bug = cve_assigned(cve_id)
+    if bug:
+        print(bug)
+    else:
+        print("%s has no LP assigned" % (cve_id))
+
+if __name__ == "__main__":
     main()
