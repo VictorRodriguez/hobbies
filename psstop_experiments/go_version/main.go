641c4c67c09f50c880e714a3d11959cc24f5f881
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2019-12-10 21:27:27 +0000

    Working POC of PSSTOP
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- psstop_experiments/go_version/main.go
+++ psstop_experiments/go_version/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 type process struct {
@@ -12,6 +16,12 @@ type process struct {
 	name   string
 	PSS_kb uint64
 	valid  int
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 // Name in => /proc/%i/comm
@@ -30,7 +40,33 @@ func main() {
 		if f.IsDir() {
 			if pid, err := strconv.Atoi(f.Name()); err == nil {
 				p := process{pid: pid}
-				fmt.Println(p.pid)
+				p.PSS_kb = 0
+
+				dat, err := ioutil.ReadFile(directory + f.Name() + "/comm")
+				check(err)
+				p.name = strings.TrimSuffix(string(dat), "\n")
+
+				file, err := os.Open(directory + f.Name() + "/smaps")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
+
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					if strings.Contains(scanner.Text(), "Pss:") {
+						s := strings.Split(scanner.Text(), ":")
+						reg, err := regexp.Compile("[^0-9]+")
+						check(err)
+						pss_s := reg.ReplaceAllString(strings.TrimSpace(s[1]), "")
+						pss, err := strconv.ParseUint(pss_s, 10, 64)
+						check(err)
+						p.PSS_kb += pss
+
+					}
+				}
+
+				fmt.Println(p.name, " PID: ", p.pid, " = ", p.PSS_kb)
 			}
 		}
 	}
