87968ddfb85b57752e572f2870d455e3831c72d4
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-02-09 02:00:01 +0000

    improve the test
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- openblas_tests/test_cblas_dgemm.c
+++ openblas_tests/test_cblas_dgemm.c
@@ -1,15 +1,34 @@
+/*
+* Test Name: cblas_dgemm
+* Based on : https://www.openblas.net/
+* Author: Victor Rodriguez
+* Gloal: Review that optimized blas lib works with presition
+*/
+
 #include <cblas.h>
 #include <stdio.h>
 
-void main()
-{
-  int i=0;
-  double A[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};         
-  double B[6] = {1.0,2.0,1.0,-3.0,4.0,-1.0};  
-  double C[9] = {.5,.5,.5,.5,.5,.5,.5,.5,.5}; 
-  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans,3,3,2,1,A, 3, B, 3,2,C,3);
+int main(){
 
-  for(i=0; i<9; i++)
+  int i=0;
+  int alpha = 1;
+  double result = 4.0;
+  double A[6] = {1.0,1.0,1.0,1.0,1.0,1.0};
+  double B[6] = {1.0,1.0,1.0,1.0,1.0,1.0};
+  double C[9] = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
+
+  cblas_dgemm(CblasColMajor,\
+	 CblasNoTrans,\
+	 CblasTrans,\
+	 3,3,2,alpha,A, 3, B, 3,2,C,3);
+
+  for(i=0; i<9; i++){
     printf("%lf ", C[i]);
+	if (C[i] != result){
+		printf("Error: Aritmetic error\n");
+		return -1;
+	}
+  }
   printf("\n");
+  printf("PASS: cblas_dgemm working as expected\n");
 }
