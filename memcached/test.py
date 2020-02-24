0804a4cbd043cc1f7efe322a04aabc1cd921b506
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-02-24 17:07:37 -0600

    Add tests for cherency of exisitng key
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- memcached/test.py
+++ memcached/test.py
@@ -6,43 +6,70 @@ import sys
 value = 'some value'
 key= 'some_key'
 
-set_flag = False
-get_flag = False
 
 
-def set_test(client):
-    global set_flag
+def set_test(client,key,value):
+    ret_val = False
     ret = client.set(key, value)
     if ret:
         print("SET PASS")
         print("Value stored on key %s = %s" % (key,value))
-        set_flag = True
+        ret_val = True
     else:
         print("SET FAIL")
+    return ret_val
 
-def get_test(client):
-    global get_flag
+def get_test(client,key):
     ret = client.get(key)
     ret_str = ret.decode("utf-8")
+    return ret_str
+
+def check_coherency(value,ret_str):
     if value in ret_str:
         print("GET PASS")
         print("Value retrieve from key %s = %s" % (key,ret_str))
-        get_flag = True
+        print("Is the same as : %s" % (value))
+        ret = True
     else:
         print("GET FAIL")
+        ret = False
+
+    return ret
+
+def test_set_get_basic(client):
+
+    ret_set = set_test(client,key,value)
+    ret_str = get_test(client,key)
+
+    ret_coherency = check_coherency(value,ret_str)
+
+    if ret_set and ret_coherency:
+        print("TEST PASS")
+    else:
+        print("TEST FAIL")
+        sys.exit(-1)
+
+def test_set_get_existing(client):
+    new_value = 'over writing'
+    ret_set = set_test(client,key,new_value)
+    ret_str = get_test(client,key)
+
+    ret_coherency = check_coherency(new_value,ret_str)
+
+    if ret_set and ret_coherency:
+        print("TEST PASS")
+    else:
+        print("TEST FAIL")
+        sys.exit(-1)
 
 def main():
 
     client = base.Client(('localhost', 11211))
-    set_test(client)
-    get_test(client)
+    test_set_get_basic(client)
+    test_set_get_existing(client)
 
-    if set_flag and get_flag:
-        print("TEST PASS")
-        sys.exit(0)
-    else:
-        print("TEST FAIL")
-        sys.exit(-1)
+    sys.exit(0)
+
 
 if __name__== "__main__":
     main()
