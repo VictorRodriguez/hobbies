3fc81d87d7cb018fd49c9976b3f2f32aed705249
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-05-19 14:55:41 -0500

    Add q-e build instructions
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

---
+++ hpcrs/q-e/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export Q_E_ARGS="CC=icc CXX=icpc F77=ifort F90=ifort FC=ifort MPIF90=mpiifort \
+ --enable-openmp=yes --enable-parallel=yes --with-scalapack=intel"
+
+# flags for icc
+export CFLAGS="$CFLAGS -xCORE-AVX512"
+export FCFLAGS="$CFLAGS -xCORE-AVX512"
+export CXXFLAGS="$CXXFLAGS -xCORE-AVX512"
+
+# Link compilers and libraries
+export BLAS_LIBS="$BLAS_LIBS"
+export LAPACK_LIBS="$LAPACK_LIBS"
+export MPI_LIBS="$MPI_LIBS"
+
+if [ ! -d "q-e" ]; then
+	git clone https://gitlab.com/QEF/q-e.git
+fi
+
+cd q-e/
+./configure
+make all
+
