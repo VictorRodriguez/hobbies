d88adf3c0379aa4ae9050eae0c0233ca03d4c16e
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-06-24 18:33:51 -0500

    check avx 512 and avx2
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- c_programing_exercises/avx_detection/builtin_cpu_supports.c
+++ c_programing_exercises/avx_detection/builtin_cpu_supports.c
@@ -6,8 +6,10 @@ int main(void) {
   printf("%d\n", __builtin_cpu_supports ("avx"));
   printf("%d\n", __builtin_cpu_supports ("avx2"));
   printf("%d\n", __builtin_cpu_supports ("avx512dq"));
-  if (__builtin_cpu_supports("avx512dq")){
-	printf("has support for avx512");		  
-  }
-
+  if (__builtin_cpu_supports("avx512dq"))
+	printf("has support for avx512");
+  else if(__builtin_cpu_supports("avx2"))
+	printf("has support for avx2");
+  else
+	printf("has support for sse4");
 }
