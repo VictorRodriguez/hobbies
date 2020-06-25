86fd7ddbd06e058cc76a2f76cc175489075fc100
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-06-25 10:06:54 -0500

    add option to measure perfomrance of ubuntu:latest
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- profiling_container/tests/run_bench.sh
+++ profiling_container/tests/run_bench.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+python3 --help &> /dev/null
+
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo "Error... Installing python"
+	apt-get update
+	apt-get install curl --assume-yes
+	apt-get install xz-utils --assume-yes
+	apt-get install python3 --assume-yes
+fi
+
 echo "Running bench"
 curl -O -L http://www.phoronix-test-suite.com/benchmark-files/pybench-2018-02-16.tar.gz
 tar -xvf pybench-2018-02-16.tar.gz
