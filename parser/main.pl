31d0de20fea0593e2d5321fa69badfb46aff4961
Victor Rodriguez <vm.rod25@gmail.com>
2020-12-01 19:41:41 -0600

    Add better version of parser
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

---
+++ parser/main.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+$x = 10;
+
+use warnings;
+
+
+my $filename = 'test.txt';
+
+open(FH, '<', $filename) or die $!;
+
+while(<FH>){
+	if ($_ =~ /f64Longitude \(deg\) = (\d+)/) {
+    	print $1;
+	}
+}
+
+close(FH);
