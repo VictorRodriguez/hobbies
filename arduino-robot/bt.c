e068fdcda64e22ab4b466a25c0bf2cc49e94755f
Victor Rodriguez <vm.rod25@gmail.com>
2021-01-01 20:07:14 -0600

    basic code for bt
    This basic code for BT -> Arduino ONE is taken from here:
    
    https://create.arduino.cc/projecthub/mayooghgirish/arduino-bluetooth-basic-tutorial-d8b737
    
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

---
+++ arduino-robot/bt.c
@@ -0,0 +1,28 @@
+/*
+ *  Bluetooh Basic: LED ON OFF - Avishkar
+ *  Coder - Mayoogh Girish
+ *  Website - http://bit.do/Avishkar
+ *  Download the App :
+ *  This program lets you to control a LED on pin 13 of arduino using a bluetooth module
+ */
+char Incoming_value = 0;                //Variable for storing Incoming_value
+void setup()
+{
+  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
+  pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
+}
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    Incoming_value = Serial.read();      //Read the incoming data and store it into variable Incoming_value
+    Serial.print(Incoming_value);        //Print Value of Incoming_value in Serial monitor
+    Serial.print("\n");        //New line
+    if(Incoming_value == '1')            //Checks whether value of Incoming_value is equal to 1
+      digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
+    else if(Incoming_value == '0')       //Checks whether value of Incoming_value is equal to 0
+      digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
+  }
+
+}
+
