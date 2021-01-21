ba8c28755ddb2ee24911eafc10049706d9556792
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2021-01-21 11:42:41 -0600

    fix % printng
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- cpu_monitor/cpu_utilization.c
+++ cpu_monitor/cpu_utilization.c
@@ -20,7 +20,7 @@ int main(void)
         fclose(fp);
 
         loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
-        printf("The current CPU utilization is : %Lf\n",loadavg);
+        printf("The current CPU utilization is : %Lf % \n",loadavg * 100 );
     }
 
     return(0);
