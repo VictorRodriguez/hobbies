ba94457cc8cc3412bc32a315f0f6f4d8a5494af6
Victor Rodriguez <vm.rod25@gmail.com>
2021-02-19 12:50:18 -0600

    plot surface example
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

---
+++ genetic_algorithms/plot_surface.py
@@ -0,0 +1,13 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.outer(np.linspace(-2, 2, 30), np.ones(30))
+y = x.copy().T # transpose
+z = np.cos(x ** 2 + y ** 2)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot_surface(x, y, z,cmap='viridis', edgecolor='none')
+ax.set_title('Surface plot')
+plt.show()
