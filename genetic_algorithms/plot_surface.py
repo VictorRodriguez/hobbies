7a964e64651dcfde6f461834887480744692b43e
Victor Rodriguez <vm.rod25@gmail.com>
2021-02-23 12:48:10 -0600

    add example of surface plot
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

--- genetic_algorithms/plot_surface.py
+++ genetic_algorithms/plot_surface.py
@@ -1,13 +1,58 @@
-from mpl_toolkits import mplot3d
 import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-x = np.outer(np.linspace(-2, 2, 30), np.ones(30))
-y = x.copy().T # transpose
-z = np.cos(x ** 2 + y ** 2)
+
+x = []
+y = []
+z = []
+
+def f(input_array):
+    global x
+    global y
+    global z
+    x.append(input_array[0])
+    y.append(input_array[1])
+    total = -np.sum(input_array)
+    z.append(total)
+    return total
+
+
+varbound=np.array([[0,10]]*2)
+
+
+algorithm_param = {'max_num_iteration': 100,\
+                   'population_size':100,\
+                   'mutation_probability':0.1,\
+                   'elit_ratio': 0.01,\
+                   'crossover_probability': 0.5,\
+                   'parents_portion': 0.3,\
+                   'crossover_type':'uniform',\
+                   'max_iteration_without_improv':None}
+
+model=ga(function=f,\
+            dimension=2,\
+            variable_type='real',\
+            variable_boundaries=varbound,\
+            algorithm_parameters=algorithm_param)
+
+model.run()
+
+print(len(x))
+print(len(y))
+print(len(z))
+
+
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 
-ax.plot_surface(x, y, z,cmap='viridis', edgecolor='none')
-ax.set_title('Surface plot')
+ax.scatter(x, y, z, c='y', marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
 plt.show()
+
+
