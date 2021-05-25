911648e81c031504b4b785805db22acb4e69219e
Victor Rodriguez <vm.rod25@gmail.com>
2021-05-25 16:55:10 -0500

    add casting example
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

--- c_programing_exercises/basic/arguments.c
+++ c_programing_exercises/basic/arguments.c
@@ -6,6 +6,7 @@ int main(int argc , char **argv){
 		printf("ok, filename %s\n", argv[1]);
 	}else{
 		printf("error no filename \n");
+		return -1;
 	}
 	return  0;
 }
---
+++ c_programing_exercises/basic/cast.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+	int input=0;
+	printf("Please provide an integer:\n");
+	scanf("%d",&input);
+	printf("floating point of value = %.1f.\n",(float)input);
+
+	return 0;
+}
