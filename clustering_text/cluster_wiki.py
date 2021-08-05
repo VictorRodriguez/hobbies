d5c913f5a7decd60c085e9bb4ab7398e0cc72d1d
Victor Rodriguez <vm.rod25@gmail.com>
2021-08-05 13:26:31 -0500

    enable print images
    Signed-off-by: Victor Rodriguez <vm.rod25@gmail.com>

--- clustering_text/cluster_wiki.py
+++ clustering_text/cluster_wiki.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import wikipedia
-articles=['Data Science','Artificial intelligence',
-          'European Central Bank',
+articles=['Data Science','Artificial intelligence','Deeplearning',
+          'European Central Bank', 'Central bank',
           'Basketball','Swimming']
 wiki_lst=[]
 title=[]
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 Sum_of_squared_distances = []
-K = range(2,5)
+K = range(2,len(articles))
 for k in K:
     km = KMeans(n_clusters=k, max_iter=200, n_init=10)
     km = km.fit(X)
@@ -43,3 +43,24 @@ model.fit(X)
 labels=model.labels_
 wiki_cl=pd.DataFrame(list(zip(title,labels)),columns=['title','cluster'])
 print(wiki_cl.sort_values(by=['cluster']))
+
+
+
+from wordcloud import WordCloud
+result={'cluster':labels,'wiki':wiki_lst}
+result=pd.DataFrame(result)
+for k in range(0,true_k):
+    s=result[result.cluster==k]
+    text=s['wiki'].str.cat(sep=' ')
+    text=text.lower()
+    text=' '.join([word for word in text.split()])
+    wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+    print('Cluster: {}'.format(k))
+    print('Titles')
+    titles=wiki_cl[wiki_cl.cluster==k]['title']
+    print(titles.to_string(index=False))
+    plt.figure()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
