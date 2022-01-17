7d5618fb089ec459f0f5910c19d02c3d385a4993
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2022-01-17 13:43:05 -0600

    add test to calculate target
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- target_calculator/target_calculator.py
+++ target_calculator/target_calculator.py
@@ -26,8 +26,12 @@ def calculate_slope_intercept(x, y, independent):
     slope = lm.params[1]
     intercept = lm.params[0]
     y_pred = slope * x[independent] + intercept
-    print(("Ecuation :  %s*x + %s") % (slope, intercept))
-    return y_pred
+    return y_pred, slope, intercept
+
+
+def calculate_target(slope, intercept, y_target):
+    x_target = (y_target - (intercept)) / slope
+    return x_target
 
 
 def parse_args(args):
@@ -35,6 +39,7 @@ def parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--dependent')
     parser.add_argument('--independent')
+    parser.add_argument('--y_target', type=int)
     parser.add_argument('--data_file_name')
     parser.add_argument('--image_name', default='linear_regression')
     args = parser.parse_args()
@@ -48,12 +53,15 @@ def main():
         df_ = pd.read_csv(args.data_file_name)
     else:
         print('Error: Please provide a filename: --data_file_name')
-    if args.dependent and args.independent:
+    if args.dependent and args.independent and args.y_target:
         y = df_[args.dependent]
         x = df_[args.independent]
         x = sm.add_constant(x)  # adding a constant
-        y_pred = calculate_slope_intercept(x, y, args.independent)
-        print(y_pred[100])
+        y_pred, slope, intercept = calculate_slope_intercept(
+            x, y, args.independent)
+        print(("Ecuation :  %s*x + %s") % (slope, intercept))
+        x_target = calculate_target(slope, intercept, args.y_target)
+        print(("Y = %s when X = %s") % (args.y_target, x_target))
         plot(x, y, args.independent, y_pred, args.image_name)
     else:
         print('Error: Please provide --dependent and --independent values')
--- target_calculator/tests/test_basic.py
+++ target_calculator/tests/test_basic.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import statsmodels.api as sm
 
-from ..target_calculator import calculate_slope_intercept
+from ..target_calculator import calculate_slope_intercept, calculate_target
 
 
 def test_calculate_slope_intercept():
@@ -13,5 +13,19 @@ def test_calculate_slope_intercept():
     y = df_[dependent]
     x = df_[independent]
     x = sm.add_constant(x)
-    y_pred = calculate_slope_intercept(x, y, independent)
+    y_pred, slope, intercept = calculate_slope_intercept(x, y, independent)
     assert y_pred[100] == 26.559266339961354
+
+
+def test_calculate_target():
+    data_file_name = 'tests/fixtures/BostonHousePrices.csv'
+    y_target = 40
+    df_ = pd.read_csv(data_file_name)
+    dependent = 'Value'
+    independent = 'Rooms'
+    y = df_[dependent]
+    x = df_[independent]
+    x = sm.add_constant(x)
+    y_pred, slope, intercept = calculate_slope_intercept(x, y, independent)
+    x_target = calculate_target(slope, intercept, y_target)
+    assert x_target == 8.203661473492458
