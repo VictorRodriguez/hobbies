d9dc2439561b525e538d921f67490f6568f51d65
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2022-07-18 11:31:30 -0500

    add level of limit
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- statistics/hamming/hamming.py
+++ statistics/hamming/hamming.py
@@ -17,13 +17,12 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
+    parser.add_argument('limit')
     args = parser.parse_args()
 
     data = read_data(args.filename)
     mean = statistics.mean(data)
     stdev = statistics.stdev(data)
-    print(mean)
-    print(stdev)
     hamming_list = []
     expected_patern = [1] * (len(data))
 
@@ -35,7 +34,12 @@ def main():
     if len(expected_patern) == len(hamming_list):
         plot_data(hamming_list)
         hd = distance.hamming(expected_patern,hamming_list)
-        print(f'Hamming distance vs expected pattern (Lower is better) = {hd}')
+        print(f'Hamming distance vs expected pattern (Lower is better) = {hd * 100} %')
+        if args.limit:
+            if float(hd*100) > float(args.limit):
+                print(f"ERROR: Hamming distance grater than limit ({args.limit} %)")
+            else:
+                print(f"PASS: Hamming distance less than limit ({args.limit} %)")
 
 if __name__ == "__main__":
     main()
