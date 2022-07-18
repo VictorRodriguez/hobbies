6aad3967557fd6de41b568935b0e3808742b4596
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2022-07-18 10:41:52 -0500

    add plot part
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- statistics/hamming/hamming.py
+++ statistics/hamming/hamming.py
@@ -2,12 +2,16 @@ import statistics
 import argparse
 from pandas import *
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
 
 def read_data(file_name):
     data = read_csv(file_name)
     instr_ret_any = data['INST_RETIRED.ANY'].tolist()
     return instr_ret_any
 
+def plot_data(data):
+    plt.plot(data)
+    plt.show()
 
 def main():
 
@@ -18,6 +22,8 @@ def main():
     data = read_data(args.filename)
     mean = statistics.mean(data)
     stdev = statistics.stdev(data)
+    print(mean)
+    print(stdev)
     hamming_list = []
     expected_patern = [1] * (len(data))
 
@@ -27,6 +33,7 @@ def main():
         else:
             hamming_list.append(0)
     if len(expected_patern) == len(hamming_list):
+        plot_data(hamming_list)
         hd = distance.hamming(expected_patern,hamming_list)
         print(f'Hamming distance vs expected pattern (Lower is better) = {hd}')
 
