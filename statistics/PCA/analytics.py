c623be324925b145a52c48bb1ace090f3195da07
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2022-08-14 09:57:10 -0500

    add tsne
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- statistics/PCA/analytics.py
+++ statistics/PCA/analytics.py
@@ -5,6 +5,8 @@ from distortion import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import sys
+from sklearn.manifold import TSNE
+
 
 
 def get_explained_variance(X_std):
@@ -37,6 +39,45 @@ def get_eigen(X_std):
     print(f'Eigenvalues \n {eig_vals}')
 
 
+def get_TSNE(df, features, test_column):
+    # Separating out the features
+    x = df.loc[:, features].values
+    # Separating out the target
+    y = df.loc[:,[test_column]].values
+    cov_mat = np.cov(x.T)
+    eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+
+    # Standardizing the features
+    X_std = StandardScaler().fit_transform(x)
+    get_eigen(X_std)
+
+    tsne = TSNE(n_components=2, verbose=1, perplexity = 10)
+    tsne_results = tsne.fit_transform(X_std)
+    print(tsne_results)
+
+    principalDf = pd.DataFrame(data = tsne_results, columns = ['TSNE 1', 'TSNE 2'])
+    finalDf = pd.concat([principalDf, df[['test_name']]], axis = 1)
+    finalDf.to_csv("tsne.csv")
+    print(finalDf)
+
+    fig = plt.figure(figsize=(16,10))
+    ax = fig.add_subplot(1,1,1)
+
+    ax.set_xlabel('TSNE 1', fontsize = 15)
+    ax.set_ylabel('TSNE 2', fontsize = 15)
+    ax.set_title('2 component TSNE', fontsize = 20)
+    ax.scatter(finalDf['TSNE 1']
+                   ,finalDf['TSNE 2']
+                   , c = 'g'
+                   , s = 50)
+
+    for i, label in enumerate(y):
+        plt.annotate(label, (finalDf['TSNE 1'][i], finalDf['TSNE 2'][i]))
+
+    ax.grid()
+    plt.savefig('tsna.png')
+    plt.show()
+
 def get_PCA(df, features, test_column):
     print(df)
     # Separating out the features
@@ -54,13 +95,13 @@ def get_PCA(df, features, test_column):
 
     # Standardizing the features
     X_std = StandardScaler().fit_transform(x)
-    #X_std = x
 
     get_eigen(X_std)
 
     get_explained_variance(X_std)
 
     pca = PCA(n_components=2)
+
     principalComponents = pca.fit_transform(X_std)
 
     principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
@@ -85,12 +126,14 @@ def get_PCA(df, features, test_column):
     plt.savefig('pca.png')
     plt.show()
 
+
 def main():
     df = pd.read_csv('results_icx_norm.csv')
     features = list(df.columns)[1:]
     test_column = list(df.columns)[0]
 
     get_PCA(df, features,test_column)
+    get_TSNE(df, features,test_column)
     calculate_elbow(pd.read_csv("pca.csv"))
 
 if __name__ == "__main__":
--- statistics/PCA/clustering.py
+++ statistics/PCA/clustering.py
@@ -3,19 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+FILE = "tsne.csv"
+clusters = 6
 
 if len(sys.argv) > 1:
-    clusters = int(sys.argv[1])
-else:
-    clusters = 6
+    FILE = sys.argv[1]
+    print(FILE)
+    clusters = int(sys.argv[2])
 
-df = pd.read_csv("pca.csv")
+df = pd.read_csv(FILE)
 test_column = list(df.columns)[3]
 print(test_column)
 
-df = pd.read_csv("pca.csv", usecols = ['principal component 1','principal component 2'])
+if "pca" in FILE:
+    df = pd.read_csv(FILE, usecols = ['principal component 1','principal component 2'])
+elif "tsne" in FILE:
+    df = pd.read_csv(FILE, usecols = ['TSNE 1','TSNE 2'])
+else:
+    sys.exit(-1)
 
-workload_name_df = pd.read_csv("pca.csv", usecols =[test_column])
+workload_name_df = pd.read_csv(FILE, usecols =[test_column])
 workload_name = workload_name_df[test_column].values
 
 kmeans = KMeans(init="k-means++", n_clusters=clusters).fit(df)
@@ -27,6 +34,7 @@ df['labels'] = labels
 df[test_column] = workload_name
 
 print(df)
+y = df.loc[:,['test_name']].values
 
 grouped_df = df.groupby("labels")
 grouped_lists = grouped_df[test_column].apply(list)
@@ -34,11 +42,21 @@ grouped_lists = grouped_lists.reset_index()
 print(grouped_lists.explode(test_column))
 grouped_lists.explode(test_column).to_csv("clusters.csv")
 
-plt.scatter(df['principal component 1'], df['principal component 2'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
 plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
-plt.title("Workload clustering based on similar pre silicon behavior")
-plt.xlabel("Principal component 1")
-plt.ylabel("Principal component 2")
+if "pca" in FILE:
+    plt.scatter(df['principal component 1'], df['principal component 2'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+    for i, label in enumerate(y):
+        plt.annotate(label, (df['principal component 1'][i], df['principal component 2'][i]))
+elif "tsne" in FILE:
+    plt.scatter(df['TSNE 1'], df['TSNE 2'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+    for i, label in enumerate(y):
+        plt.annotate(label, (df['TSNE 1'][i], df['TSNE 2'][i]))
+else:
+    sys.exit(-1)
+
+plt.xlabel("component 1")
+plt.ylabel("component 2")
 plt.grid()
+plt.savefig('clustering.png')
 plt.show()
 
