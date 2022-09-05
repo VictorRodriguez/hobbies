c6441a7985c16fe96716c47e44fcb0bf4c1780f8
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2022-09-05 15:41:11 -0500

    fix hello example
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- flask/hello.py
+++ flask/hello.py
@@ -1,10 +1,12 @@
+# an object of WSGI application
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__)   # Flask constructor
 
-# routing the decorator function hello_name
-@app.route('/hello/<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/')
+def hello():
+    return 'HELLO'
 
-if __name__ == '__main__':
-   app.run(debug = True)
+if __name__=='__main__':
+   app.run()
