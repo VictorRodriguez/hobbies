60f6464ae89194f37a56b7f12cd945fb7ecc5f24
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2024-05-22 23:09:24 +0000

    add elbow method
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- rust/pca_example/src/main.rs
+++ rust/pca_example/src/main.rs
@@ -2,6 +2,7 @@
 //use ndarray::array;
 use linfa::traits::{Fit, Predict};
 use linfa_reduction::Pca;
+use linfa_clustering::{KMeans};
 
 fn main() {
     // Input data
@@ -9,18 +10,45 @@ fn main() {
     //               [3.0, 4.0]];
     //let dataset = Dataset::from(x);
 
-    // Create Dataset
     let dataset = linfa_datasets::iris();
 
-    // Create PCA instance with the number of components
     let pca = Pca::params(2)
-        .fit(&dataset).unwrap();
+        .fit(&dataset)
+        .expect("PCA failing");
 
-    // Project data
-    let transformed = pca.predict(dataset);
+    let transformed = pca.predict(dataset.clone());
 
-    // Print the transformed data
     println!("Transformed data:\n{:?}", transformed);
 
+    let cluster_range = 1..10;
+    let mut inertias = Vec::new();
+
+    for n_clusters in cluster_range.clone() {
+        let _model = KMeans::params(n_clusters)
+        .fit(&dataset)
+        .expect("KMeans fitted");
+        let _inertia = _model.inertia();
+        inertias.push((n_clusters, _inertia));
+        println!("{:?}",_inertia);
+    }
+
+    let optimal_k = find_elbow(&inertias);
+    println!("Optimal number of clusters (elbow point): {}", optimal_k);
+
+}
+
+fn find_elbow(inertias: &[(usize, f64)]) -> usize {
+    let mut prev_inertia = f64::MAX;
+    let mut best_k = 1;
+    for &(k, inertia) in inertias {
+        let diff = prev_inertia / inertia;
+        if diff < 1.5 {
+            // If the ratio falls below 1.5, consider it as the elbow point
+            best_k = k;
+            break;
+        }
+        prev_inertia = inertia;
+    }
+    best_k
 }
 
