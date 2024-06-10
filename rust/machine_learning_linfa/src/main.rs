40c0ac8c14326ed84aedc5eb5b3bd90fb50342e4
VictorRodriguez <victor.rodriguez.bahena@intel.com>
2024-06-10 15:36:13 -0600

    Enable find optimal number of cluster
    Also tested with
    
    https://www.kaggle.com/datasets/binovi/wholesale-customers-data-set
    https://www.kaggle.com/datasets/yasserh/wine-quality-dataset/discussion/324451
    
    Signed-off-by: VictorRodriguez <victor.rodriguez.bahena@intel.com>

--- rust/machine_learning_linfa/src/main.rs
+++ rust/machine_learning_linfa/src/main.rs
@@ -6,14 +6,54 @@ use ndarray::{Axis, ArrayBase, OwnedRepr, Dim};
 use polars::prelude::*;
 use ndarray_rand::rand::SeedableRng;
 use rand_xoshiro::Xoshiro256Plus;
+use ndarray::Array2;
+use std::env;
 use plotters::prelude::*;
 
+fn find_optimal_number_of_clusters(inertias: &[(usize, f64)]) -> usize {
+    // Calculate differences between consecutive inertias
+    let differences: Vec<f64> = inertias
+        .iter()
+        .map(|&(n, inertia)| inertia)
+        .collect::<Vec<_>>()
+        .windows(2)
+        .map(|w| w[1] - w[0])
+        .collect();
+
+    // Calculate second differences
+    let second_differences: Vec<f64> = differences
+        .windows(2)
+        .map(|w| w[1] - w[0])
+        .collect();
+
+    // Find index of maximum second difference
+    let elbow_index = second_differences
+        .iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+        .map(|(i, _)| i);
+
+    // Return optimal number of clusters (adding 2 because differences is 1 element shorter than inertias)
+    elbow_index.unwrap() + 2
+}
+
+
 fn main() {
+
+	let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <file_path> <column to exclude>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let column = &args[2];
+
     // Our random number generator, seeded for reproducibility
     let  rng = Xoshiro256Plus::seed_from_u64(42);
 
     // Read the iris dataset using Polars
-    let df = CsvReader::from_path("/Users/vrodri3/devel/hobbies/rust/machine_learning_linfa/iris.csv")
+    let df = CsvReader::from_path(file_path)
         .unwrap()
         .infer_schema(None)
         .has_header(true)
@@ -21,14 +61,40 @@ fn main() {
         .unwrap();
 
 
-    let features= df.to_ndarray::<Float64Type>(IndexOrder::C);
+    let filtered_df = df.drop(column).unwrap(); // Drop the excluded column from the DataFrame
+    let features = filtered_df.to_ndarray::<Float64Type>(IndexOrder::C);
+
+    //let features= df.to_ndarray::<Float64Type>(IndexOrder::C);
 
     // Create a dataset
     let dataset = DatasetBase::from(features.unwrap());
+	println!("Printing Dataset");
+	println!("{:?}",dataset);
 
     // Configure the KMeans algorithm
-    let n_clusters = 3; // Assuming we want to identify 3 clusters
-    let model = KMeans::params_with(n_clusters, rng, LInfDist)
+    //let n_clusters = 3; // Assuming we want to identify 3 clusters
+
+    let max_clusters = 10;
+    let mut inertias = Vec::new();
+    let cluster_range = 1..=max_clusters;
+
+    println!("\nInertias per cluster:");
+    for n_clusters in cluster_range.clone() {
+        let model = KMeans::params_with(n_clusters, rng.clone(), LInfDist)
+            .max_n_iterations(200)
+            .tolerance(1e-5)
+            .fit(&dataset.clone())
+            .expect("KMeans fitted");
+        let inertia = model.inertia();
+        inertias.push((n_clusters, inertia));
+        println!("Inertia with {} clusters = {:?}", n_clusters, inertia);
+    }
+
+    let optimal_clusters = find_optimal_number_of_clusters(&inertias);
+
+    println!("optimal_clusters = {optimal_clusters}");
+
+    let model = KMeans::params_with(optimal_clusters, rng, LInfDist)
         .max_n_iterations(200)
         .tolerance(1e-5)
         .fit(&dataset)
@@ -59,7 +125,8 @@ fn main() {
     let _ = chart.configure_mesh().draw();
 
     // Define colors for each cluster
-    let colors = [RED, BLUE, GREEN];
+    let colors = [RED, BLUE, GREEN, YELLOW];
+
 
     let records: ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>> = predictions.records().clone();
     let targets = predictions.targets();
