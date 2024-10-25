903ba26ba8816303da9f34419ad91c695571dc37
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2024-10-25 18:13:14 +0000

    add bassic Docerfile
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- sde-training/workload_timing.c
+++ sde-training/workload_timing.c
@@ -8,10 +8,15 @@
 void cpu_intensive_task(int duration_seconds) {
     volatile double result = 0.0;
 	time_t start_time = time(NULL);
+
+    // Print the raw numeric value of start_time
+    printf("Start time (raw): %ld\n", (long)start_time);
+
 	while (time(NULL) - start_time < duration_seconds) {
 		for (int i = 0; i < 1000000; i++) {
 			result += i * 0.1;
 		}
+    	printf("Time (raw): %ld\n", (long)time(NULL));
 	}
 	printf("\nresult = %d",result);
 }
