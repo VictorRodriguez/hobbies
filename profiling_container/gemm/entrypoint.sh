058ec0f152bfce42dcc7e551eb9d39cad45f0ece
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2026-03-19 20:02:58 +0000

    change to basic build no iwps
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- profiling_container/gemm/entrypoint.sh
+++ profiling_container/gemm/entrypoint.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 set -eu
 
-ARCH="${ARCH:-avx2}"
+ARCH=${ARCH:-avx2}
+ITERS=${ITERS:-10}
+M=${M:-256}
+N=${N:-256}
+K=${K:-256}
 
 case "$ARCH" in
   avx2)
-    exec /app/gemm_stress_avx2_sniper "$@"
+    BIN="/app/gemm_stress_avx2"
     ;;
   avx512)
-    exec /app/gemm_stress_avx512_sniper "$@"
+    BIN="/app/gemm_stress_avx512"
     ;;
   *)
-    echo "Error: ARCH must be 'avx2' or 'avx512'" >&2
+    echo "Error: ARCH must be avx2 or avx512"
     exit 1
     ;;
 esac
+
+echo "Running GEMM benchmark"
+echo "ISA   : $ARCH"
+echo "iters : $ITERS"
+echo "M,N,K : $M $N $K"
+echo ""
+
+exec "$BIN" "$ITERS" "$M" "$N" "$K"
